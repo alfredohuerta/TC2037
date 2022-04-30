487dca89b941082238b06b47e9fcb537afe23211
Pantro Huerta <pantro12@live.com.mx>
2022-04-30 04:59:37 -0500

    Se terminó la actividad integradora 3.4 Resaltador de sintaxis

--- Racket/Resaltador de sintaxis/Actividad Integradora 3.4 Resaltador de sintaxis.rkt
+++ Racket/Resaltador de sintaxis/Actividad Integradora 3.4 Resaltador de sintaxis.rkt
@@ -2,20 +2,18 @@
 (require 2htdp/batch-io)
 (require racket/trace)
 
-(define Input "test.cpp")
-(define Output "testOutput.txt")
+;; Erick Alfredo García Huerta - A01708119
+;; Alan Fernando Razo Peña - A01703350 
 
-;; Listas de tokens
-(define variables '("variables"))
-(define operadores '("operadores"))
-(define reales '("reales"))
-(define especiales '("especiales"))
-(define comentarios '("comentarios"))
-(define reservadas '("reservadas"))
-(define indexReservadas '("[alignas|alignof|and|and_eq|asm|atomic_cancel|atomic_commit|atomic_noexcept|auto|bitand|bitor|bool|break|case|catch|char|char8_t|char16_t|char32_t|class|compl|concept|const|consteval|constexpr|constinit|const_cast|continue|co_await|co_return|co_yield|decltype|default|delete|do|double|dynamic_cast|else|enum|explicit|export|extern|false|float|for|friend|goto|if|inline|int|long|mutable|namespace|new|noexcept|not|not_eq|nullptr|operator|or|or_eq|private|protected|public|reflexpr|register|reinterpret_cast|requires|return|short|signed|sizeof|static|static_assert|static_cast|struct|switch|synchronized|template|this|thread_local|throw|true|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while|xor|xor_eq)]"))
-(define empty '("NADA"))
+;; Definición de archivos a utilizar
+(define Input "test.cpp")
+(define Output "Final.html")
+
+;; Encabezado del archivo html
+(define header "<!DOCTYPE html><html lang='esp-mx'> <head> <meta charset='UTF-8'> <title>Resaltador de sintaxis</title> <style>body{color: beige;}.PReservadas{color: blue;}.Variables{color: blueviolet;}.Operadores{color: aqua;}.Reales{color: chartreuse;}.Especiales{color: crimson;}.Comentarios{color: gold;}</style> </head> <body>")
 
 ;; Extrae los caracteres del documento y las almacena en una lista. InputFile->characterList
+;; Complejidad O(1) debido a que no utiliza ningún ciclo y sus funciones se aplican al mismo tiempo a todos los elementos de la lista gracias a la función map
 (define file->list-of-chars
   (lambda (filename)
     (flatten
@@ -24,6 +22,7 @@
 
 #|Función que obteiene una lista de caracteres (loc) y dos listas vacías (aux, result), recorre toda la lista de caracteres almacenando los valores en el auxiliar,
 y cuando se encuentra con un espacio vacío (#\tab, #\space, #\newline) convierte la lista de caracteres que ha extraído en un string que se almacena en resultados|#
+;; Complejidad O(n) debido al uso de ciclos recursivos cuya duración depende de la longitud del arreglo de caracteres, ya que el caso base para la ruptura del ciclo es el vaciado de dicha lista.
 (define char->string
   (lambda (loc aux result)
     (cond
@@ -48,46 +47,55 @@ y cuando se encuentra con un espacio vacío (#\tab, #\space, #\newline) conviert
                                        (append aux (cons (car loc) '()))
                                        result)])))
 
+;; Función auxiliar que simplifica la obtención de la lista de strings
+;; Complejidad O(1)+O(n)= O(n) debido a que la complejidad de esta función está ligada a la complejidad de la función char->string
 (define string-list
   (lambda (filename)
-    (reverse
-     (char->string (file->list-of-chars filename) '() '()))))
+    (char->string (file->list-of-chars filename) '() '())))
 
-;; Se crean los regex match que contendran las expresiones regulares y filtrará los tokens
+;; Se crean los regex match que contendran las expresiones regulares y filtrará los tokens que serán escritos y almacenados en un string global una vez se haya hecho el regexp-match
+;; Complejidad O(n) debido al uso de ciclos recursivos cuya duración depende de la longitud del arreglo de strings, ya que el caso base para la ruptura del ciclo es el vaciado de dicha lista.
 (define matches
   (lambda (strList result)
     (cond
       [(empty? strList)
-       result]
-      
+       (string-append result "</body></html>")]
+
       ;; Palabras reservadas
-      [(regexp-match #rx"[alignas|alignof|and|and_eq|asm|atomic_cancel|atomic_commit|atomic_noexcept|auto|bitand|bitor|bool|break|case|catch|char|char8_t|char16_t|char32_t|class|compl|concept|const|consteval|constexpr|constinit|const_cast|continue|co_await|co_return|co_yield|decltype|default|delete|do|double|dynamic_cast|else|enum|explicit|export|extern|false|float|for|friend|goto|if|inline|int|long|mutable|namespace|new|noexcept|not|not_eq|nullptr|operator|or|or_eq|private|protected|public|reflexpr|register|reinterpret_cast|requires|return|short|signed|sizeof|static|static_assert|static_cast|struct|switch|synchronized|template|this|thread_local|throw|true|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while|xor|xor_eq)]" (car strList))
-       ]
-      
-      ;; Variables
-      [(regexp-match #rx"[a-zA-Z][a-zA-Z_0-9]*" (car strList))
-       ]
+      [(regexp-match #rx"alignas|alignof|and|and_eq|asm|atomic_cancel|atomic_commit|atomic_noexcept|auto|bitand|bitor|bool|break|case|catch|char|char8_t|char16_t|char32_t|class|compl|concept|const|consteval|constexpr|constinit|const_cast|continue|co_await|co_return|co_yield|decltype|default|delete|do|double|dynamic_cast|else|enum|explicit|export|extern|false|float|for|friend|goto|if|inline|int|long|mutable|namespace|new|noexcept|not|not_eq|nullptr|operator|or|or_eq|private|protected|public|reflexpr|register|reinterpret_cast|requires|return|short|signed|sizeof|static|static_assert|static_cast|struct|switch|synchronized|template|this|thread_local|throw|true|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while|xor|xor_eq" (car strList))
+       (matches (cdr strList) (string-append "<div class= 'PReservadas'>Palabra reservada: " (car strList) "</div>" result))]
       
       ;; Operadores
       [(regexp-match #rx"[\\*|\\/|\\^|\\=|\\+|\\-]" (car strList))
-       ]
+       (matches (cdr strList) (string-append "<div class= 'Operadores'>Operador: " (car strList) "</div>" result))]
       
       ;; Números reales
       [(regexp-match #rx"-*[0-9]+\\.[0-9]+([E][-*][0-9]+)?|-*[0-9]+(\\.[0-9]+)?" (car strList))
-       ]
+       (matches (cdr strList) (string-append "<div class= 'Reales'>Número real: " (car strList) "</div>" result))]
       
       ;; Caracteres especiales
-      [(regexp-match #rx"[\\(\\)\\#\\;\\.\\{\\}\\<\\>]" (car strList))
-       ]
+      [(regexp-match #rx"[\\)\\(\\#\\;\\.\\}\\{\\<\\>]" (car strList))
+       (matches (cdr strList) (string-append "<div class= 'Especiales'>Caracter especial: " (car strList) "</div>" result))]
+      
+      ;; Variables
+      [(regexp-match #rx"[a-zA-Z][a-zA-Z_0-9]*" (car strList))
+       (matches (cdr strList) (string-append "<div class= 'Variables'>Variable: " (car strList) "</div>" result))]
       
       ;; Comentarios
       [(regexp-match #rx"//.*$" (car strList))
-       ]
+       (matches (cdr strList) (string-append "<div class= 'Comentarios'>Comentario: " (car strList) "</div>" result))]
       
       [else
-       (matches (cdr strList) result])))
+       (matches (cdr strList) result)])))
 
-(define test
-  (lambda (filename)
-    (matches (string-list filename) '("reservadas") '("variables") '("operadores") '("reales")'("especiales")'("comentarios") '())))
-;;
+;; Función auxiliar que permite obtener el string final con todos los datos del html
+;; Complejidad O(1)+O(n)= O(n) debido a que hace uso de una función con complejidad O(n) depende de ella completamente para funcionar.
+(define finalList
+  (lambda (inputFile)
+    (string-append header (matches (reverse(string-list inputFile)) ""))))
+
+;; Función que escribe los datos del string global en un archivo html
+;; Complejidad O(1)+O(n)= O(n) debido a que hace uso de una función con complejidad O(n) depende de ella completamente para funcionar.
+(define writing
+  (lambda (inputFile outputFile)
+    (write-file outputFile (finalList inputFile))))
